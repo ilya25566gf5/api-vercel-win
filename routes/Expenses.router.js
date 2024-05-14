d8c72d8d0ab9864dc8513016908340a9ed575bc2
ilya25566gf5 <ilya1313120@gmail.com>
2024-05-15 03:08:27 +0500

    Все остальное

--- routes/Expenses.router.js
+++ routes/Expenses.router.js
@@ -4,7 +4,7 @@ const ExpensesController = require("../controller/Expenses.controller");
 
 router.get("/", ExpensesController.getAll);// метод для вывода данных
 router.post("/", ExpensesController.addExpens); // метод добавления
-router.put("/:id", ExpensesController.updateExpens); //  метод обновления
+router.put("/", ExpensesController.updateExpens); //  метод обновления
 router.delete("/:id_income", ExpensesController.deleteExpens); // метод  удаления
 
 module.exports = router;
